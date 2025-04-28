0c957a7098e4950d67627dd72890069f2cb97f5b
Arunkumar <101180226+AK514@users.noreply.github.com>
2025-04-28 16:50:12 +0530

    Add files via upload

---
+++ CommercialCentralizedSystem/src/test/java/com/CommercialCentralizedSystem/CommercialCentralizedSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CommercialCentralizedSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommercialCentralizedSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
